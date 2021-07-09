0d9dc03d0b48c96f80042841be322a037d16ceb3
sercala97 <sercala97@gmail.com>
2021-07-09 23:40:09 +0200

    Nueva rama para series no estacionarias creada. He probado el metodo propuesto por (katz 2013) para calcular la location.

---
+++ R/fitGEV_NS.R
@@ -0,0 +1,30 @@
+
+library(fitdistrplus)
+library(ismev)
+library(extRemes)
+library(actuar)
+data("danishmulti")
+plot(danishmulti$Contents)
+plot(wind$Albany)
+y <- venice$r8
+glm(Fort$Prec ~ Fort$obs)
+tseries::adf.test(dowjones$Index)
+TFMevt::fitGEV(dowjones$Index, c(1,1,1))
+
+library(readr)
+data <-read_csv("R/2644206.csv", col_types = cols(DATE = col_date(format = "%Y-%m-%d")))
+
+plot(data$DATE,data$PRCP)
+y <- data$PRCP
+
+plot(density(y))
+library(tidyverse)
+data <- data %>%
+  filter(PRCP != is.na(PRCP))
+fit_fev <- fevd(y)
+return.level(fit_fev)
+fit <-TFMevt::fitGEV(y,c(1,1,1))
+TFMevt::return_level(year = 5,fit)
+
+plot(data$DATE,data$PRCP)
+abline(h=10.079)
