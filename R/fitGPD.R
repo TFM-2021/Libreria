23c2dfb17970f2e22646004c6bbb86a1188a156e
sercala97 <sercala97@gmail.com>
2021-07-11 17:27:27 +0200

    Pareto hecha con matriz de covarianzas

--- R/fitGPD.R
+++ R/fitGPD.R
@@ -12,14 +12,39 @@
 
 fitGPD = function(data,threshold){
 
-  n <- length(data)
+  data_u <- data[data > threshold]
+  exceso <- data_u - threshold
 
   eq = function(par){
+    shape <- par[2]
 
-    alpha <- par[1]
-    xm <- min(data)
-    -(n*log(alpha)+n*log(xm) - (alpha +1)*sum(log(data))) # https://en.wikipedia.org/wiki/Pareto_distribution
-    #-n/(sum(log(data/xm)))
+    scale <- par[1]
+    #-(n*log(alpha)+n*log(xm) - (alpha +1)*sum(log(data))) # https://en.wikipedia.org/wiki/Pareto_distribution
+    # -n/(sum(log(data/xm)))
+    #-(n*log(alpha) + n*log(xm)-(alpha +1)*sum(log(data)))# https://math.stackexchange.com/questions/1399209/how-to-find-the-maximum-likelihood-estimators-of-parameters-in-the-pareto-distri/1399292
+
+
+    -(-length(data_u) * logb(scale) - (1 + 1/shape) * sum(logb(1 + (shape * exceso)/scale)))
   }
-  optim(c(0.1),fn = eq)
+  optimizacion <- optim(c(0.1,0.1),fn = eq,hessian = T)
+
+  resultados_fit <- tibble("Parametro"= c("scale", "shape"),
+                           "Valores_optimos"= optimizacion$par)
+
+  verosimilitud <- c(optimizacion$value)
+
+  # RESUMEN------------------------------------------------------------------
+  list("Valores_optimos"= tibble("Parametro"= c("scale", "shape"),
+                                 "Valores_optimos"= optimizacion$par),
+
+       "Negative_Log_Likelihood"=tibble(verosimilitud),
+
+       "Desviaciones_tipicas_estimadas" = tibble("scale"=sqrt(solve(optimizacion$hessian)[1,1]),
+                                                 "shape"=sqrt(solve(optimizacion$hessian)[2,2])),
+
+       "Matriz_covarianzas" = tibble("scale"=solve(optimizacion$hessian)[1,],
+                                     "shape"=solve(optimizacion$hessian)[2,]))
 }
+
+
+
