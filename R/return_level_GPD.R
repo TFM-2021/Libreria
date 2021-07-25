5bdd343b0b8c8ae9192fd37dc0607df078230591
sercala97 <sercala97@gmail.com>
2021-07-25 23:22:06 +0200

    pp plot y (return level completado)

--- R/return_level_GPD.R
+++ R/return_level_GPD.R
@@ -8,7 +8,12 @@
 
 
 
-return_level_GPD = function(model_fit, data, threshold, year){
+return_level_GPD = function(model_fit,
+                             data,
+                             threshold,
+                             year,
+                             mode="calculation"){
+
   scale <-model_fit$Valores_optimos[1,2]
   shape <- model_fit$Valores_optimos[2,2]
 
@@ -32,21 +37,80 @@ return_level_GPD = function(model_fit, data, threshold, year){
 
   V[1,] <- c(rate*(1-rate)/length(data),0,0)
 
+  if(mode=="calculation"){
+    m <- 365* year
 
-  m <- 365* year
+    x_m <- as.numeric(threshold+scale/shape*(((m*rate)^shape)-1))
 
-  x_m <- as.numeric(threshold+scale/shape*(((m*rate)^shape)-1))
-
-  d_1 <- as.numeric((scale*m^shape)*rate^(shape - 1))
-  d_2 <- as.numeric(shape^-1*((m*rate)^shape-1))
-  d_3 <- as.numeric(-scale*shape^-2*((m*rate)^shape-1)+scale*shape^-1*((m*rate)^shape)*log(m*rate))
+    d_1 <- as.numeric((scale*m^shape)*rate^(shape - 1))
+    d_2 <- as.numeric(shape^-1*((m*rate)^shape-1))
+    d_3 <- as.numeric(-scale*shape^-2*((m*rate)^shape-1)+scale*shape^-1*((m*rate)^shape)*log(m*rate))
 
 
-  x_m_delta <- c(d_1,d_2,d_3)
+    x_m_delta <- c(d_1,d_2,d_3)
 
-  Var <- t(matrix(x_m_delta)) %*% V %*% matrix(x_m_delta)
+    Var <- t(matrix(x_m_delta)) %*% V %*% matrix(x_m_delta)
 
-  c(x_m + 1.96*sqrt(Var),
-    x_m - 1.96*sqrt(Var))
+    return(c(x_m + 1.96*sqrt(Var),
+             x_m,
+             x_m - 1.96*sqrt(Var)))
+
+  }
+
+  if (mode=="plot") {
+    upper_band<- as.vector(NULL)
+    return_level <- as.vector(NULL)
+    lower_band <- as.vector(NULL)
+
+
+    for (year_ in 1:year) {
+
+      m <- 365 * year_
+
+      x_m <- as.numeric(threshold+scale/shape*(((m*rate)^shape)-1))
+
+      d_1 <- as.numeric((scale*m^shape)*rate^(shape - 1))
+      d_2 <- as.numeric(shape^-1*((m*rate)^shape-1))
+      d_3 <- as.numeric(-scale*shape^-2*((m*rate)^shape-1)+scale*shape^-1*((m*rate)^shape)*log(m*rate))
+
+
+      x_m_delta <- c(d_1,d_2,d_3)
+
+      Var <- t(matrix(x_m_delta)) %*% V %*% matrix(x_m_delta)
+
+
+      upper_band[year_] <- x_m + 1.96*sqrt(Var)
+      return_level[year_] <- x_m
+      lower_band[year_]<-  x_m - 1.96*sqrt(Var)
+
+
+    }
+
+    as.tibble(upper_band,
+              return_level,
+              lower_band) %>%
+      ggplot()+
+      geom_line(aes(1:year, return_level), color="tomato")+
+      geom_line(aes(1:year, upper_band))+
+      geom_line(aes(1:year, lower_band))+
+      scale_x_log10()+
+      labs(title =  "Return levels GPD: ",
+           subtitle = paste0("threshold: ", threshold,
+                             " scale: ", round(scale,2),
+                             " shape: ", round(shape,2)),
+           x = "Return year",
+           y = "Return level",
+           color = NULL) +
+
+      theme_minimal()
+
+  }
+
 
 }
+
+
+
+
+
+
